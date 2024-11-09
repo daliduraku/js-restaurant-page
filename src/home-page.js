046ff334012bb86d808d37b09f38df5c823c5561
daliduraku <mesherbetmolla@gmail.com>
2024-11-08 22:33:25 -0500

    initial start of the main page, created an img directory to store the images needed

---
+++ src/home-page.js
@@ -0,0 +1,16 @@
+// home page
+function createPage() {
+    // content div
+    const content = document.getElementById('content');
+
+    // main section
+    const main = document.createElement('main');
+
+    const description = document.createElement('p');
+    description.textContent = "Experience the finest dining with our exquisite menu crafted by amazing chefs."
+    main.appendChild(description);
+
+
+    // img
+    
+}

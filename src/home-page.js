8a9ff2b3f2ea0b6061980f4130ed04f0fa7d534b
daliduraku <mesherbetmolla@gmail.com>
2024-11-08 22:58:23 -0500

    added the hours of operations for the restaurant to the main section of the home page

--- src/home-page.js
+++ src/home-page.js
@@ -3,6 +3,11 @@ function createPage() {
     // content div
     const content = document.getElementById('content');
 
+    // name of the restaurant
+    const restaurantName = document.createElement('h1')
+    restaurantName.textContent = "Peaches"
+    main.append(restaurantName);
+
     // main section
     const main = document.createElement('main');
 
@@ -12,5 +17,28 @@ function createPage() {
 
 
     // img
-    
+    const image = document.createElement('img')
+    image.src = "./img/restaurant-home-page.jpg";
+    image.alt = "A view of our restaurant";
+    main.appendChild(image);
+
+    // hours of operation
+    const schedule = document.createElement('section');
+    const scheduleTitle = document.createElement('h2');
+    scheduleTitle.textContent = "Hours"
+    schedule.appendChild(scheduleTitle)
+
+    const hourOperations = document.createElement('ul');
+
+    const hours = ["Sunday: 8am - 8pm", "Monday: 10am - 11pm", "Tuesday: 10am - 11pm", "Wednesday: 10am - 11pm", "Thursday: 10am - 11pm", "Friday: 10am - 11pm", "Friday: 10am - 11pm", "Saturday: 10am - 11pm"]
+
+    hours.forEach(hour => {
+        const listItem = document.createElement('li');
+        listItem.textContent = hour;
+        hourOperations.appendChild(listItem);
+    });
+
+    schedule.appendChild(hourOperations);
+    main.appendChild(schedule);
+
 }

5f6a3a3737b19d2167e9b8354b923ad25c02bef7
daliduraku <mesherbetmolla@gmail.com>
2024-11-11 14:15:36 -0500

    contact info for the contact page added

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -16,6 +16,33 @@ export function loadContactPage() {
     contactTitle.textContent = "Contact Us";
     main.appendChild(contactTitle);
 
-    //
+    //contact information
+    const contactInfo = document.createElement('section');
+    contactInfo.classList.add('contact-info');
+
+    // location info
+    const location = document.createElement('h2');
+    location.textContent = "Our Location";
+    const address = document.createElement('p');
+    address.textContent = "9404 Bald Hill LaneBrooklyn, NY 11228";
+    contactInfo.appendChild(location);
+    contactInfo.appendChild(address);
+
+    // phone number info
+    const phone = document.createElement('h2');
+    phone.textContent = "Phone";
+    const phoneNumber = document.createElement('p');
+    phoneNumber.textContent = "(123)-456-7890";
+    contactInfo.appendChild(phone);
+    contactInfo.appendChild(phoneNumber);
+    
+    // email address info
+    const email = document.createElement('h2');
+    email.textContent = "Email";
+    const emailAddress = document.createElement('p');
+    emailAddress.textContent = "info@peachesrestaurant.com";
+    contactInfo.appendChild(email);
+    contactInfo.appendChild(emailAddress);
+
 
 }
